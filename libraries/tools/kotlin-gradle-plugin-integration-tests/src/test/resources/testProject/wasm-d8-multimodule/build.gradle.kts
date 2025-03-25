3e4486875e5c00119daef7e525eb9358e573dabb
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2025-03-25 19:21:45 +0100

    [Wasm] Add multimodule integration test

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/wasm-d8-multimodule/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("org.jetbrains.kotlin.multiplatform")
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+kotlin {
+    wasmJs {
+        binaries.executable()
+        d8 {
+            runTask {
+                val directory = this.inputFileProperty.get().asFile.parentFile
+                inputFileProperty.set(File(directory, "app.mjs"))
+            }
+        }
+        val multiModuleMode = if ("MULTIMODULE_MODE_MASTER_ENABLE".isEmpty()) "slave" else "master"
+        compilerOptions {
+            freeCompilerArgs.add("-Xwasm-multimodule-mode=$multiModuleMode")
+        }
+    }
+}
+
+tasks.register<Copy>("masterCopy") {
+    from(File(project.buildDir, "wasm"))
+    into(File(project.buildDir, "master"))
+}
+
+tasks.register<Copy>("masterCopyBack") {
+    from(File(project.buildDir, "master"))
+    into(File(project.buildDir, "wasm"))
+}
