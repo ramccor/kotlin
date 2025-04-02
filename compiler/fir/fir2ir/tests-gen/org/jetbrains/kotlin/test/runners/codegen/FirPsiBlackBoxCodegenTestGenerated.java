d8abfe9c33816662d623f5d8de82644678b07b05
Grigorii Solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2025-04-02 15:36:59 +0300

    Move tests from codegen/box/ to diagnostics/testsWithJvmBackend
    Tests triangleWithFlexibleTypeAndSubstitution4.kt and
    immutableName.kt can't be executed because they fail with both
    K1 and K2 due to the backend fix of the issue KT-13712, therefore
    they are moved to diagnostics.

--- compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/codegen/FirPsiBlackBoxCodegenTestGenerated.java
+++ compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/codegen/FirPsiBlackBoxCodegenTestGenerated.java
@@ -36078,12 +36078,6 @@ public class FirPsiBlackBoxCodegenTestGenerated extends AbstractFirPsiBlackBoxCo
     }
 
     @Test
-    @TestMetadata("immutableName.kt")
-    public void testImmutableName() {
-      runTest("compiler/testData/codegen/box/javaInterop/immutableName.kt");
-    }
-
-    @Test
     @TestMetadata("inheritanceWithWildcard.kt")
     public void testInheritanceWithWildcard() {
       runTest("compiler/testData/codegen/box/javaInterop/inheritanceWithWildcard.kt");
@@ -36267,12 +36261,6 @@ public class FirPsiBlackBoxCodegenTestGenerated extends AbstractFirPsiBlackBoxCo
     @TestMetadata("syntheticPropOverriddenGetter.kt")
     public void testSyntheticPropOverriddenGetter() {
       runTest("compiler/testData/codegen/box/javaInterop/syntheticPropOverriddenGetter.kt");
-    }
-
-    @Test
-    @TestMetadata("triangleWithFlexibleTypeAndSubstitution4.kt")
-    public void testTriangleWithFlexibleTypeAndSubstitution4() {
-      runTest("compiler/testData/codegen/box/javaInterop/triangleWithFlexibleTypeAndSubstitution4.kt");
     }
 
     @Test
