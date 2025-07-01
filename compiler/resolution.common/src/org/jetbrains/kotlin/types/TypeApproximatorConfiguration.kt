7fe6c3746812c45e7e63c437c9596889496ad268
Oleg Makeev <oleg.makeev@jetbrains.com>
2025-07-01 19:50:48 +0200

    [Analysis API] Clean up and refactor the super type approximation infrastructure in `KaTypeProvider`
    The previous API `approximateToSuperPublicDenotable` got deprecated, as it had a misleading naming (it has nothing to do with public declarations) and `approximateLocalTypes` parameter had shady meaning and wasn't actually consistent with the provided documentation.
    It was replaced with `approximateToDenotableSupertype`, which always performs local types approximation, however, has a way to choose whether local types can be approximated to denotable local types or not.
    This is done by introducing a new filter for local types in `TypeApproximatorConfiguration`.
    
    ^KT-68387

--- compiler/resolution.common/src/org/jetbrains/kotlin/types/TypeApproximatorConfiguration.kt
+++ compiler/resolution.common/src/org/jetbrains/kotlin/types/TypeApproximatorConfiguration.kt
@@ -7,6 +7,7 @@ package org.jetbrains.kotlin.types
 
 import org.jetbrains.kotlin.builtins.functions.AllowedToUsedOnlyInK1
 import org.jetbrains.kotlin.config.LanguageFeature
+import org.jetbrains.kotlin.types.TypeApproximatorConfiguration.IntermediateApproximationToSupertypeAfterCompletionInK2.convertToNonRawVersionAfterApproximationInK2
 import org.jetbrains.kotlin.types.model.*
 
 abstract class TypeApproximatorConfiguration {
@@ -41,6 +42,15 @@ abstract class TypeApproximatorConfiguration {
     open val intersectionStrategy: IntersectionStrategy get() = IntersectionStrategy.TO_COMMON_SUPERTYPE
     open val approximateIntersectionTypesInContravariantPositions get() = false
     open val approximateLocalTypes get() = false
+
+    /**
+     * Defines additional condition for local type approximation.
+     * Should return false if the current local type should be considered as a final approximation.
+     * This check is triggered for every found local supertype of the initial type (including the type itself).
+     * Note that [approximateLocalTypes] should be true for this to have any effect.
+     */
+    open fun shouldApproximateLocalType(ctx: TypeSystemInferenceExtensionContext, type: KotlinTypeMarker): Boolean =
+        true
 
     /**
      * Is only expected to be true for FinalApproximationAfterResolutionAndInference
