9671131e77ca2fc744b7f3f3d3467b067476eb16
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-07-07 12:37:08 +0300

    [FIR] Add a `local-inline-local` test for `NOT_YET_SUPPORTED_IN_INLINE`
    ^KT-77986
    ^KT-25341

---
+++ compiler/testData/diagnostics/testsWithJsStdLib/localInsideInlineInsideLocal.fir.kt
@@ -0,0 +1,26 @@
+// ISSUE: KT-77986, KT-25341
+// DIAGNOSTICS: -NOTHING_TO_INLINE
+
+inline fun topLevelInlineFun() {
+    <!NOT_YET_SUPPORTED_IN_INLINE!>fun<!> localFun() {}
+    localFun()
+
+    inline fun localInlineFun() {
+        <!NOT_YET_SUPPORTED_IN_INLINE!>fun<!> localFun() {}
+        localFun()
+    }
+    localInlineFun()
+}
+
+fun topLevelFun() {
+    inline fun localInlineFun() {
+        <!NOT_YET_SUPPORTED_IN_INLINE!>fun<!> localFun() {}
+        localFun()
+    }
+    localInlineFun()
+}
+
+fun main() {
+    topLevelInlineFun()
+    topLevelFun()
+}
