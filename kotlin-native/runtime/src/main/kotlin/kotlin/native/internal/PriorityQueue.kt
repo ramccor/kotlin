45ba7df5b7ce872b7b25f52a3f6b5c19a1812dd1
Aleksei.Glushko <aleksei.glushko@jetbrains.com>
2025-07-09 18:25:53 +0200

    [K/N] Internal implementation of PriorityQueue ^KT-79037
        To be used internaly e.g. by workers

---
+++ kotlin-native/runtime/src/main/kotlin/kotlin/native/internal/PriorityQueue.kt
@@ -0,0 +1,131 @@
+package kotlin.native.internal
+
+public class PriorityQueue<T>(
+        initialCapacity: Int,
+        private val comparator: Comparator<T>
+) : AbstractMutableCollection<T>() {
+
+    public constructor(comparator: Comparator<T>) : this(DEFAULT_INITIAL_CAPACITY, comparator)
+
+    public companion object {
+        private const val DEFAULT_INITIAL_CAPACITY = 11
+
+        public fun <T : Comparable<T>> minimal(initialCapacity: Int = DEFAULT_INITIAL_CAPACITY): PriorityQueue<T> =
+                PriorityQueue(initialCapacity) { a, b -> a.compareTo(b) }
+
+        public fun <T : Comparable<T>> maximal(initialCapacity: Int = DEFAULT_INITIAL_CAPACITY): PriorityQueue<T> =
+                PriorityQueue(initialCapacity) { a, b -> b.compareTo(a) }
+    }
+
+    private val elements = ArrayList<T>(initialCapacity)
+
+    private var modCount: Int = 0
+
+    override val size: Int
+        get() = elements.size
+
+    override fun iterator(): MutableIterator<T> {
+        var expectedModCount = modCount
+        val ordered = ArrayList(elements).also { it.sortWith(comparator) }
+        return object : MutableIterator<T> {
+            private var nextIndex = 0
+            override fun hasNext(): Boolean = nextIndex < ordered.size
+            override fun next(): T {
+                if (modCount != expectedModCount) throw ConcurrentModificationException()
+                return ordered[nextIndex++]
+            }
+
+            override fun remove() {
+                if (modCount != expectedModCount) throw ConcurrentModificationException()
+                this@PriorityQueue.removeAt(nextIndex - 1)
+                expectedModCount = modCount
+            }
+        }
+    }
+
+    public fun addFirst(element: T): Boolean {
+        elements.add(element)
+        siftUp(elements.size - 1)
+        return true
+    }
+
+    override fun add(element: T): Boolean = addFirst(element)
+
+    public fun firstOrNull(): T? {
+        return if (isEmpty()) null else elements[0]
+    }
+
+    public fun first(): T {
+        return firstOrNull() ?: throw NoSuchElementException("PriorityQueue is empty")
+    }
+
+    public fun removeFirstOrNull(): T? {
+        if (isEmpty()) return null
+        return removeAt(0)
+    }
+
+    public fun removeFirst(): T = removeFirstOrNull() ?: throw NoSuchElementException("PriorityQueue is empty")
+
+    override fun clear() {
+        elements.clear()
+    }
+
+    override fun remove(element: T): Boolean {
+        val index = elements.indexOf(element)
+        if (index == -1) return false
+        removeAt(index)
+        return true
+    }
+
+    // FIXME we can do better for removeAll/retainAll
+
+    private fun removeAt(index: Int): T {
+        val removedElement = elements[index]
+        if (index == elements.size - 1) {
+            elements.removeAt(index)
+        } else {
+            elements[index] = elements.removeAt(elements.size - 1)
+            siftDown(index)
+        }
+        return removedElement
+    }
+
+    private fun leftChild(index: Int): Int = 2 * index + 1
+    private fun rightChild(index: Int): Int = leftChild(index) + 1
+    private fun parent(index: Int): Int = (index - 1) / 2
+
+    private operator fun T.compareTo(other: T): Int = comparator.compare(this, other)
+
+    private fun siftUp(start: Int) {
+        val startElement = elements[start]
+        var child = start
+        while (child > 0) {
+            val parent = parent(child)
+            if (startElement >= elements[parent]) break
+            elements[child] = elements[parent]
+            child = parent
+        }
+        elements[child] = startElement
+    }
+
+    private fun siftDown(start: Int) {
+        val startElement = elements[start]
+        var parent = start
+        val firstLeaf = elements.size / 2
+        while (parent < firstLeaf) {
+            val left = leftChild(parent)
+            val right = rightChild(parent)
+
+            val leastChild = if (right < elements.size && elements[right] < elements[left]) {
+                right
+            } else {
+                left
+            }
+
+            if (startElement <= elements[leastChild]) break
+            elements[parent] = elements[leastChild]
+            parent = leastChild
+        }
+        elements[parent] = startElement
+    }
+}
---
+++ kotlin-native/runtime/test/native/internal/PriorityQueueTest.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package test.native.internal
+
+import kotlin.native.internal.PriorityQueue
+import kotlin.test.*
+
+class PriorityQueueTest {
+    val testData = listOf(108, 42, 37, 4, 8, 15, 16, 23, 42)
+
+    @Test
+    fun addAndFirstElement() {
+        val queue = PriorityQueue.minimal<Int>()
+        queue.add(testData.first())
+        assertFalse(queue.isEmpty())
+        assertEquals(1, queue.size)
+        assertEquals(testData.first(), queue.first())
+    }
+
+    @Test
+    fun minHeap() {
+        val queue = PriorityQueue.minimal<Int>()
+        testData.forEach { queue.add(it) }
+
+        assertEquals(
+                testData.sorted(),
+                generateSequence { queue.removeFirstOrNull() }.toList()
+        )
+    }
+
+    @Test
+    fun maxHeap() {
+        val queue = PriorityQueue.maximal<Int>()
+        testData.forEach { queue.add(it) }
+
+        assertEquals(
+                testData.sortedDescending(),
+                generateSequence { queue.removeFirstOrNull() }.toList()
+        )
+    }
+}
