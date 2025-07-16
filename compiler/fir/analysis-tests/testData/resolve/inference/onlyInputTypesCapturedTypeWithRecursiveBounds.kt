6072064acc083c289c29b6ed0bc39a050a695169
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2025-07-16 12:30:49 +0200

    fixup! [Language] Drop feature JavaTypeParameterDefaultRepresentationWithDNN

--- compiler/fir/analysis-tests/testData/resolve/inference/onlyInputTypesCapturedTypeWithRecursiveBounds.kt
+++ compiler/fir/analysis-tests/testData/resolve/inference/onlyInputTypesCapturedTypeWithRecursiveBounds.kt
@@ -1,7 +1,6 @@
 // RUN_PIPELINE_TILL: BACKEND
 // FULL_JDK
 // WITH_STDLIB
-// LANGUAGE: 
 import java.util.EnumMap
 
 interface I
--- compiler/testData/diagnostics/tests/j+k/overrideWithTypeParameterReverse.kt
+++ compiler/testData/diagnostics/tests/j+k/overrideWithTypeParameterReverse.kt
@@ -1,6 +1,5 @@
 // RUN_PIPELINE_TILL: BACKEND
 // FIR_IDENTICAL
-// LANGUAGE: 
 // FILE: Super.kt
 interface Super {
     fun <B : Any?> foo(klass: Class<B & Any>): B
